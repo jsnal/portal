d044511e1239460f13d1b021981e6712176aff0a
jsnal <jasonlongball@gmail.com>
2019-05-28 00:16:41 +0000

    new paste and directory css support

---
+++ paste/paste-listener.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Listen for paste pushes.
+
+SEND_OUTPUT_PORT="2757";
+
+while true ; do
+  x=$(nc -l -q 0 -p $SEND_OUTPUT_PORT)
+  filename=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+  echo "$x" > posts/"$filename".txt
+  paste --compile
+  echo -e "File found at $(date). Assigned $filename."
+done
--- paste/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-paste -l -p 2757 -c -t "Jason's Pastebin" --search-bar true
