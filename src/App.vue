f9c0e67075461a9c1e9ddf9e8715dff6325dd1a0
jsnal <jasonlongball@gmail.com>
2020-03-22 21:08:10 -0400

    revamp navbar with a simple top nav system

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,40 @@
 <template>
   <b-container id="app" fluid>
     <title>{{ getTitle() }}</title>
-    <b-row>
-      <div id="header">
-        <router-link to="/">Jason Long</router-link>
-        <NavMenuHide />
-      </div>
-    </b-row>
-    <b-row>
-      <b-col sm="3" id="navmenu-column">
-        <NavMenu :navmenuTemplate=navmenu />
-      </b-col>
-      <b-col sm="9" id="router-column">
-        <router-view/>
-      </b-col>
-    </b-row>
-    <b-row>
-      <div id="footer"></div>
-    </b-row>
+    <div id="header">
+      <b-navbar id="navbar" toggleable="lg" type="dark" variant="dark">
+        <b-navbar-brand id="navbar-brand" to="/">Jason Long</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item-dropdown text="Projects">
+              <b-dropdown-item
+                v-for="(project, index) in projects"
+                :href="'/projects/' + index"
+                :key="project">
+                {{ index }}
+              </b-dropdown-item>
+            </b-nav-item-dropdown>
+            <b-nav-item to="/records">Records</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <router-view/>
+    <footer id="footer">
+    </footer>
   </b-container>
 </template>
 
 <script>
-import NavMenu from '@/components/NavMenu.vue';
-import NavMenuHide from '@/components/NavMenuHide.vue';
+import { PROJECTURLS } from './data/constants';
 
 export default {
   components: {
-    NavMenu,
-    NavMenuHide,
   },
   data() {
     return {
-      navmenu: [
-        {
-          name: 'projects',
-          header: true,
-          href: '#',
-          sublinks: {
-            items: [
-              {
-                name: 'i3wm',
-                href: '/projects/i3wm',
-              },
-              {
-                name: 'paste-light',
-                href: '/projects/paste-light',
-              },
-              {
-                name: 'vim-serape',
-                href: '/projects/vim-serape',
-              },
-              {
-                name: 'abbs',
-                href: '/projects/abbs',
-              },
-              {
-                name: 'portal',
-                href: '/projects/portal',
-              },
-            ],
-          },
-        },
-        {
-          name: 'records',
-          header: false,
-          href: '/records',
-        },
-        {
-          name: 'about',
-          header: false,
-          href: '/about',
-        },
-      ],
+      projects: PROJECTURLS,
     };
   },
   methods: {
@@ -80,7 +42,6 @@ export default {
       if (this.$route.name === 'home') {
         return 'jasonlong/';
       }
-
       return `/${this.$route.name}`;
     },
   },
@@ -88,35 +49,53 @@ export default {
 </script>
 
 <style>
-#header {
-  background: #ccc;
-  min-height: 1em;
-  width: 100%;
+#app {
+  padding: 0px;
 }
 
-#header > a {
+#navbar {
+  height: 1.5em;
+  background-color: #ccc !important;
+}
+
+#navbar-brand {
   font-weight: bold;
   text-decoration: none;
-  font-size: 14px;
+  font-size: 16px;
   color: #000;
   margin: 0 6px;
 }
 
-#header > a:hover {
-  color: #666;
+.nav-link, .dropdown-item {
+  color: #000 !important;
+  font-size: 14px;
+  font-weight: bold !important;
 }
 
-#container {
-  display: flex;
-  width: 100%;
+.nav-link:hover, #navbar-brand:hover {
+  color: #666 !important;
 }
 
-#nav-column {
-  margin: 0 auto;
+.dropdown-item {
+  padding: 0.1rem .5rem !important;
 }
 
-#router-column {
-  margin: 0 auto;
+@media (max-width: 991px) {
+  #navbar {
+    height: inherit;
+  }
+
+  .navbar-toggler {
+    border: 0px !important;
+  }
+
+  .navbar-toggler-icon {
+    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='30' height='30' viewBox='0 0 30 30'%3e%3cpath stroke='rgba(0, 0, 0, 1)' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e") !important;
+  }
+
+  #navbar-brand {
+    margin: 0px !important;
+  }
 }
 
 #footer {
