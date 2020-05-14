5ffaa311df9476fa5bb52decd2819123724fbddd
jsnal <jasonlongball@gmail.com>
2020-05-13 21:58:31 -0400

    feat(server): add `/getNoteByBlobHash` endpoint
    the `/getNoteByBlobHash` endpoint will return a note based on the blob
    hash supplied. I added express-validator to check that the body of the
    request makes sense and is in the proper format.
    
    I need to make a proper logging helper class to work with logging to the
    console and throwing errors back from express.

--- server/src/routers/notes.js
+++ server/src/routers/notes.js
@@ -1,17 +1,25 @@
 import express from 'express';
+import { body, validationResult } from 'express-validator';
 import Note from '../models/notes';
 
 const notes = express.Router();
 
+function checkInputError(req, res) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(422).json({
+      message: 'failure',
+      status: 422,
+      errors: errors.array()
+    });
+    return true;
+  }
+  return false;
+}
+
 notes.get('/tags', (req, res) => {
   res.json({ message: "TAGS" });
 })
-
-// This is a test
-notes.get('/getContent', async (req, res) => {
-  const html = await Note.find({blobHash: 'add6c48ec844252f646c63dd838f28ef4913aeb0'})
-  res.send(html[0].html);
-});
 
 /*
  * GET - Total note count
@@ -28,5 +36,38 @@ notes.get('/getNoteCount', async (req, res) => {
 notes.get('/getAllNotes', async (req, res) => {
   res.json(await Note.find());
 });
+
+/*
+ * POST - Get a note by a specific blob hash
+ */
+notes.post('/getNoteByBlobHash', [
+  body('blobHash')
+  .trim()
+  .not()
+  .isEmpty()
+  .withMessage('blobHash can not be empty')
+  .matches(/^[A-Za-z0-9]+$/)
+  .withMessage('blobHash must be alphanumeric only'),
+], async (req, res) => {
+  if (checkInputError(req, res)) return null;
+
+  try {
+    const note = await Note.find({blobHash: req.body.blobHash}).exec().then((note) => note[0]);
+    if (note.length === 0 || note == null)
+      throw `unable to find blobHash ${req.body.blobHash}`;
+
+    res.status(200).json(note);
+  } catch (err){
+    console.log(err);
+    res.status(400).json({
+      message: 'failure',
+      errors: err,
+    });
+  }
+});
+
+notes.post('/getNotesByGroup', [
+  body('group')
+])
 
 export default notes;
