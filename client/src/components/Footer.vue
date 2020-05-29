29e91a6e35e06e1bbbfbb810797c40b6aceef01d
jsnal <jasonlongball@gmail.com>
2020-05-28 23:03:07 -0400

    feat(client): finish a basic footer
    Finish a basic footer. Will need to be updated along the way.

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -3,18 +3,27 @@
     <div class="row center-xs">
       <div class="col-xs-2">
         <div class="box">
-          <h6>Site</h6>
-          <router-link to="/projects">Projects</router-link>
+          <p>Site</p>
+          <ul>
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/projects">Projects</router-link></li>
+            <li><router-link to="/notes">Notes</router-link></li>
+          </ul>
         </div>
       </div>
       <div class="col-xs-2">
         <div class="box">
-          <h6>External</h6>
+          <p>External</p>
+          <ul>
+            <li><a href="https://github.com/jsnal" target="_blank">Github</a></li>
+            <li><a href="mailto:jasonlongball@gmail.com">Email</a></li>
+            <li><a href="https://www.linkedin.com/in/jason-long-aab1ba181/" target="_blank">LinkedIn</a></li>
+          </ul>
         </div>
       </div>
       <div class="col-xs-2">
-        <div class="box">
-          <h6>Made by Jason Long</h6>
+        <div id="colophon" class="box">
+          <p>Made by Jason Long using VueJS, ExpressJs, and MongoDB</p>
         </div>
       </div>
     </div>
@@ -24,12 +33,39 @@
 <script></script>
 
 <style scoped>
-h6 {
-  /* text-align: center; */
+p {
+  font-size: .8em;
+  color: #000000;
+  margin-bottom: 0;
+}
+
+ul {
+  list-style: none;
+  padding-left: 0;
+  margin-top: 0;
+}
+
+li {
+  padding-top: .5em;
+}
+
+a {
+  font-size: .8em;
+  text-decoration: none;
+  color: #1480a2;
+}
+
+a:hover {
+  text-decoration: underline;
+  color: #1480a2;
 }
 
 .row {
   width: 100%;
+}
+
+#colophon {
+  text-align: left;
 }
 
 #footer {
