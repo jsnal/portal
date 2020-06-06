9273c6e0cbe95b1f15de3c2ab2bcacf219bb7d5b
jsnal <jasonlongball@gmail.com>
2020-06-05 22:59:49 -0400

    feat(deploy): continue work on the docker build

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,2 +1,11 @@
-export const CONTENT = './server/content';
-export const MONGOURI = 'mongodb://172.27.0.1/portal';
+// TODO: Figure out a better way of doing this... dotenv maybe?
+let MONGOURI;
+let CONTENT = './server/content';
+
+if (process.env.NODE_ENV === 'dev') {
+  MONGOURI = 'mongodb://127.0.0.1/portal';
+} else {
+  MONGOURI = 'mongodb://mongodb_container/portal';
+}
+
+export { CONTENT, MONGOURI };
