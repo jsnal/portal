7c0f549f831061ad3b3eb7329a35a9d34e902aa8
jsnal <jasonlongball@gmail.com>
2020-06-13 22:18:04 -0400

    feat(client): fix dev environment after deploy
    Some of the changes I made for deployment broke my dev environment.

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import 'babel-polyfill';
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import { PORT } from './constants';
 import notes from './routers/notes';
 import secret from './routers/secret';
 import logger, { morganFormat } from './utils/logger';
@@ -9,14 +10,13 @@ import connectMongo from './mongoose';
 
 connectMongo();
 
-const PORT = 3000;
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(morgan((tokens, req, res) => morganFormat(tokens, req, res)));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://jasonlong.xyz");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
--- server/src/constants.js
+++ server/src/constants.js
@@ -1,3 +1,4 @@
 // TODO: Figure out a better way of doing this... dotenv maybe?
+export const PORT = 3000;
 export const MONGOURI = process.env.NODE_ENV === 'dev' ? 'mongodb://127.0.0.1/portal' : 'mongodb://mongodb_container/portal';
 export const CONTENT = './server/content';
