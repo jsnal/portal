294b5e4f9e4365688f3456117b3542963d86b29e
jsnal <jasonlongball@gmail.com>
2020-06-13 23:27:15 -0400

    fix(server): fix failing lint test

--- server/src/app.js
+++ server/src/app.js
@@ -15,9 +15,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan((tokens, req, res) => morganFormat(tokens, req, res)));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
