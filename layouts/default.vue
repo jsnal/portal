20e4abdf69f3005f909a849117ac7c8cbb6ff431
Jason Long <jasonlongball@gmail.com>
2023-01-17 17:43:16 -0500

    Add titles for each of the pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,38 @@
 <template>
-  <div id="app">
-    <Navbar />
-    <div class="container portal-body">
-      <Nuxt />
+    <div id="app">
+        <Navbar />
+        <div class="container portal-body">
+            <Nuxt />
+        </div>
+        <Footer />
     </div>
-    <Footer />
-  </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            title_tabs: [
+                { slug: 'index', name: 'Portal' },
+                { slug: 'wiki', name: 'Portal: Wiki' },
+                { slug: 'tags', name: 'Portal: Tags' },
+                { slug: 'projects', name: 'Portal: Projects' },
+                { slug: 'resume', name: 'Portal: Resume' },
+            ]
+        }
+    },
+    head() {
+        return {
+            title: (this.title_tabs.find(t => t.slug === this.$route.name || t.slug + "-slug" === this.$route.name) || {}).name
+        }
+    }
+}
+</script>
 
 <style>
 body {
-  height: 100%;
-  margin: 0px;
+    height: 100%;
+    margin: 0px;
 }
 </style>
 
--- layouts/error.vue
+++ layouts/error.vue
@@ -3,3 +3,13 @@
         <h1>404: Page Not Found!</h1>
     </div>
 </template>
+
+<script>
+export default {
+    head() {
+        return {
+            title: "Portal: 404"
+        }
+    }
+}
+</script>
